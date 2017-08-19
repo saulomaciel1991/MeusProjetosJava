b315062add0adf424128da3712a4f189ea39b428
Saulo <saulomcm@gmail.com>
2017-08-19 13:50:16 -0300

    Atualizações na classe principal Atividade 02 - Heranças

--- Atividade02PSCN/src/Main.java
+++ Atividade02PSCN/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Funcionario f = new Funcionario("09285259456","Saulo Maciel",(short)26,3700.0);
+		Funcionario f = new Funcionario("12345678900","Saulo M.",(short)26,3700.0);
 		FuncionarioChefe fc = new FuncionarioChefe("12312312345", "Billy Kane", (short)22, 200.0, 2200.0, f);
 		FuncionarioHorario fh = new FuncionarioHorario("45645645678", "Yori Yagami", (short)30, 7.58, 220.0);
 		
