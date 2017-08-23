35b4b503a5efae3df933f2b23433770c57116b49
Saulo <saulomcm@gmail.com>
2017-08-23 20:51:28 -0300

    Implementa todos os métodos da classe Player
    Todos os métodos foram testados

--- AtividadeAprofundamento01/src/basicas/Player.java
+++ AtividadeAprofundamento01/src/basicas/Player.java
@@ -1,26 +1,52 @@
 package basicas;
 
+import java.util.ArrayList;
+
 import util.EletronicoDesligadoException;
 
-public class Player<E> {
+public abstract class Player<E> {
 
 	private Integer numeroMusicaAtual;
 	private Integer tempoMusicaAtual;
+	private ArrayList<Musica> musicas;
 
 	public void play() throws EletronicoDesligadoException {
 		this.setNumeroMusicaAtual(1);
+		System.out.println("Executando a música: " + musicas.get(this.getNumeroMusicaAtual() - 1).getNomeMusica());
+
 	}
 
 	public void stop() throws EletronicoDesligadoException {
-
+		this.setNumeroMusicaAtual(0);
+		System.out.println("Player parado");
 	}
 
 	public void avancarMusica() throws EletronicoDesligadoException {
+		Integer tamanho = musicas.size();
+		Integer num = this.getNumeroMusicaAtual();
 
+		if (tamanho > num) {
+			this.setNumeroMusicaAtual(this.getNumeroMusicaAtual() + 1);
+			num = this.getNumeroMusicaAtual();
+			System.out.println(
+					num + " - " + musicas.get(num - 1).getNomeMusica() + " - " + musicas.get(num - 1).getNomeArtista());
+		} else {
+			System.out.println("Você está na útima música da playlist");
+		}
 	}
 
 	public void recuarMusica() throws EletronicoDesligadoException {
+		
+		Integer num = this.getNumeroMusicaAtual();
 
+		if (num > 1) {
+			this.setNumeroMusicaAtual(this.getNumeroMusicaAtual() - 1);
+			num = this.getNumeroMusicaAtual();
+			System.out.println(
+					num + " - " + musicas.get(num - 1).getNomeMusica() + " - " + musicas.get(num - 1).getNomeArtista());
+		}else{
+			System.out.println("Você está na primeira musica da playlist");
+		}
 	}
 
 	public Integer getNumeroMusicaAtual() {
@@ -39,4 +65,11 @@ public class Player<E> {
 		this.tempoMusicaAtual = tempoMusicaAtual;
 	}
 
+	public ArrayList<Musica> getMusicas() {
+		return musicas;
+	}
+
+	public void setMusicas(ArrayList<Musica> musicas) {
+		this.musicas = musicas;
+	}
 }
--- AtividadeAprofundamento01/src/teste/TesteEletronicos.java
+++ AtividadeAprofundamento01/src/teste/TesteEletronicos.java
@@ -10,30 +10,44 @@ public class TesteEletronicos {
 	public static void main(String[] args) {
 
 		// Declaração das variaveis
-		AparelhoDeSom xyz = new AparelhoDeSom();		
+		AparelhoDeSom xyz = new AparelhoDeSom();
 		CD cd = new CD();
+		Player<Object> fita = new Player<Object>() {
+		};
 		Musica m1 = new Musica("Todo Carnaval tem seu fim", "Los Hermanos", "Marcelo Camelo", 252);
 		Musica m2 = new Musica("A flor", "Los Hermanos", "Rodrigo Amarante", 210);
 		Musica m3 = new Musica("Retrato Pra Iaiá", "Los Hermanos", "Marcelo Camelo", 282);
 		Musica m4 = new Musica("Assim será", "Los Hermanos", "Marcelo Camelo", 194);
-		Musica m5 = new Musica("Casa pré-fabricada", "Los Hermanos", "Marcelo Camelo", 245);
+		Musica m5 = new Musica("Casa pré-fabricada", "Los Hermanos", "Marcelo Camelo", 245);		
 		ArrayList<Musica> musicas = new ArrayList<>();
 
-		// Chamadas a metodos		
+		// setando os valores
 		musicas.add(m1);
 		musicas.add(m2);
 		musicas.add(m3);
 		musicas.add(m4);
 		musicas.add(m5);
+		
+		fita.setMusicas(musicas);
 		cd.setNomeAlbum("Bloco do eu sozinho");
 		cd.setAnoLancamento(2001);
 		cd.setMusicas(musicas);
 		cd.setNomeArtista("Los Hermanos");
 		xyz.setMarca("Sony");
-		xyz.setPotencia(60.0);		
+		xyz.setPotencia(60.0);
+		xyz.setPlayer(fita);
+
+		// Executando as funções
+
 		xyz.ligar();
-		
+		try {
+			xyz.getPlayer().play();			
 			
+		} catch (EletronicoDesligadoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
