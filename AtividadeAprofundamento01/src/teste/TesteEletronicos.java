6568d69de1aa48855670f7744f3db37949f59715
Saulo <saulomcm@gmail.com>
2017-08-30 13:15:41 -0300

    Implementa classe USBPlayer

--- AtividadeAprofundamento01/src/teste/TesteEletronicos.java
+++ AtividadeAprofundamento01/src/teste/TesteEletronicos.java
@@ -11,7 +11,7 @@ public class TesteEletronicos {
 		// Declaração das variaveis
 		AparelhoDeSom xyz = new AparelhoDeSom();
 		CD cd = new CD();
-		CDPlayer cdplayer = new CDPlayer();
+		//CDPlayer cdplayer = new CDPlayer();
 		USBPlayer usbplayer = new USBPlayer();
 		Musica m1 = new Musica("Todo Carnaval tem seu fim", "Los Hermanos", "Marcelo Camelo", 252);
 		Musica m2 = new Musica("A flor", "Los Hermanos", "Rodrigo Amarante", 210);
