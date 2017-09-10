b13118a4bb41192bd6059a92296def57e1353e1a
Saulo <saulomcm@gmail.com>
2017-09-10 18:24:14 -0300

    Alterações pequenas na classe main

--- Atividade04PSCN/src/gui/GUIUsuario.java
+++ Atividade04PSCN/src/gui/GUIUsuario.java
@@ -18,11 +18,25 @@ public class GUIUsuario {
 		u.setEmail("bia2017@hotmail.com");
 		fc.cadastrarUsuario(u, confirmacaoSenha);
 
+		Usuario u2 = new Usuario();
+		u2.setCodigo(00006);
+		u2.setNome("Karen Andrade");
+		u2.setLogin("Karen");
+		u2.setSenha("123456");
+		String confirmacaoSenha2 = "123456";
+		u2.setEmail("karen@hotmail.com");
+		fc.cadastrarUsuario(u2, confirmacaoSenha2);
+
 		System.out.println("");
 
 		String login = "Bianca";
 		String senha = "123456";
 		fc.fazerLogin(login, senha);
+		System.out.println("");
+		
+		for (Usuario usuario : fc.listar()) {
+			System.out.println(usuario);
+		}
 
 	}
 
